63e3e9ce76d93135d6d8930872e42cd39c65fb89
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-15 13:39:40 -0500

    add data, scripts for data

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n VehiclesPerms
+sfdx force:apex:execute -f scripts/init.cls
 sfdx force:org:open
