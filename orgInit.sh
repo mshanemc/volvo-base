f0eddee4c700ff132bb9e95be27a91c3f77dc998
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-27 15:26:09 -0600

    auto-activate the theme

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,6 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:apex:execute -f scripts/init.cls
+sfdx shane:org:reauth -r
+sfdx shane:theme:activate -n Volvo
 sfdx force:org:open
